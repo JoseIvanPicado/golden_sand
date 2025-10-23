28f975cd319a635786036134d63a84601639575d
Jose Ivan Centonp <josecenteno@Mac.mshome.net>
2025-10-23 09:42:27 -0600

    Feax: Correction payment_table

--- database/migrations/2025_10_23_144016_create_payment_methods_table.php
+++ database/migrations/2025_10_23_144016_create_payment_methods_table.php
@@ -12,9 +12,29 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('payment_method', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('method_name');
+            $table->string('description');
+            $table->string('provider');
+            $table->integer('account_number');
+            $table->string('status');
+            $table->date('creation_date');
+            $table->date('expiration_date');
+            $table->string('receiver_name');
+            $table->integer('amount');
+            $table->string('mail'):
+            $table->integer('commission_fee');
+
+            $table->integer('payments_id')->unsigned();
+            $table->foreign('payments_id')->references('id')->on
+            ('payments')->onDelete('cascade')->onUpdate('cascade');
+
+            $table->integer('product_id')->unsigned();
+            $table->foreign('product_id')->references('id')->on
+            ('products')->onDelete('cascade')->onUpdate('cascade');
+
             $table->timestamps();
-        });
+        }); 
     }
 
     /**
