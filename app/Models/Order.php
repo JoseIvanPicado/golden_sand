caa12381bd2270d616ecb6a7508e65ceaffeeac5
JoseIvanPicado <Jose.Picado8@tecnacional.edu.ni>
2025-10-23 21:49:33 -0600

    Fix: correction of migrations

---
+++ app/Models/Order.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $table = 'orders';
+
+    protected $fillable = [
+        'order_status',
+        'order_date',
+        'order_time',
+        'delivery_type',
+        'delivery_address',
+        'estimated_time',
+        'quantity',
+        'unit_price',
+        'total_amount',
+        'delivery_status',
+        'client_id',
+        'order_details_id'];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function order_detail()
+    {
+        return $this->belongsTo(Order_detail::class);
+    }
+}
