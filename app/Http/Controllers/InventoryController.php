607a68315576df4b3154e668e29a8bc6e55968f6
emeliimejiagarcia <emele.mejia@tecnacional.edu.ni>
2025-10-24 10:58:44 -0600

    feat: "creación de controlador inventario"

--- app/Http/Controllers/InventoryController.php
+++ app/Http/Controllers/InventoryController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Inventory;
+use App\Http\Requests\InventoryRequest;
+use App\Models\Product;
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class InventoryController extends Controller
@@ -11,7 +14,8 @@ class InventoryController extends Controller
      */
     public function index()
     {
-        //
+        $inventories = Inventory::with('product','employee')->paginate(5);
+        return view('inventories.index', compact ('inventories'));
     }
 
     /**
@@ -19,46 +23,60 @@ class InventoryController extends Controller
      */
     public function create()
     {
-        //
+        $inventories = new Inventory();
+        $products = Product::all();
+        $employees = Employee::all();
+        return view('inventories.create', compact('inventories','products','employees'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(InventoryRequest $request)
     {
-        //
+        Inventory::create($request->validated());
+        return redirect()->route('inventories.index')->with('success', 'Inventario creado.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $inventories = Inventory::find($id);
+        $products = Product::all();
+        $employees = Employee::all();
+        return view('inventories.show', compact('inventories','products','employees'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(int $id)
     {
-        //
+        $inventories = Inventory::find($id);
+        $products = Product::all();
+        $employees = Employee::all();
+        return view('inventories.edit', compact('inventories','products','employees'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(InventoryRequest $request, int $id)
     {
-        //
+        $inventories = Inventory::find($id);
+        $inventories->update($request->validated());
+        return redirect()->route('inventories.index')->with('updated', 'Inventario actualizado.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        $inventories = Inventory::find($id);
+        $inventories->delete();
+        return redirect()->route('inventories.index')->with('deleted', 'Inventario eliminado.');
     }
 }
