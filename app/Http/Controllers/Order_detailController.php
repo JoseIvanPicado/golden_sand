ebd1ab77ddffdb5ddb3a89cc96527cf5962aef11
emeliimejiagarcia <emele.mejia@tecnacional.edu.ni>
2025-10-24 10:50:46 -0600

    chore: "eliminar controlador mal escrito"

--- app/Http/Controllers/Order_detailController.php
+++ app/Http/Controllers/Order_detailController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Order_detail;
+use App\Http\Requests\Order_detailRequest;
+use App\Models\Payment;
+use App\Models\Order;
 class Order_detailController extends Controller
 {
     /**
@@ -11,7 +14,8 @@ class Order_detailController extends Controller
      */
     public function index()
     {
-        //
+        $order_details = Order_detail::with('order','payment')->paginate(5);
+        return view('order_details.index', compact ('order_details'));
     }
 
     /**
@@ -19,46 +23,60 @@ class Order_detailController extends Controller
      */
     public function create()
     {
-        //
+        $order_details = new Order_detail();
+        $payments = Payment::all();
+        $orders = Order::all();
+        return view('order_details.create', compact('order_details','payments','orders'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Order_detailRequest $request)
     {
-        //
+        Order_detail::create($request->validated());
+        return redirect()->route('order_details.index')->with('success', 'Detalle del pedido creado.');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $order_details = Order_detail::find($id);
+        $payments = Payment::all();
+        $orders = Order::all();
+        return view('order_details.show', compact('order_details','payments','orders'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(int $id)
     {
-        //
+        $order_details = Order_detail::find($id);
+        $payments = Payment::all();
+        $orders = Order::all();
+        return view('order_details.edit', compact('order_details','payments','orders'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Order_detailRequest $request, int $id)
     {
-        //
+        $order_details = Order_detail::find($id);
+        $order_details->update($request->validated());
+        return redirect()->route('order_details.index')->with('updated', 'Detalle del pedido actualizado.');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(int $id)
     {
-        //
+        $orders_details = Order_detail::find($id);
+        $orders_details->delete();
+        return redirect()->route('order_details.index')->with('deleted', 'Detalle del pedido eliminado.');
     }
 }
